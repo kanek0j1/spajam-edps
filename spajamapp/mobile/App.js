48c38d3d7dfe9999b07639b1a528b8007668d6ee
checker2nd <ne231146@senshu-u.jp>
2025-08-30 17:08:27 +0900

    メニューバー表示
    react-navigationなど追加した

--- spajamapp/mobile/App.js
+++ spajamapp/mobile/App.js
@@ -1,11 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function RecordScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Record!</Text>
+    </View>
+  );
+}
+
+function TaskScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Task!</Text>
+    </View>
+  );
+}
+
+function ManualScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Manual!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          const icons = {
+            ホーム: 'home',
+            記録: 'pencil',
+            タスク: 'file-document-outline',
+            マニュアル: 'information'
+          };
+
+          return (
+            <Icon
+              name={icons[route.name]}
+              color={color}
+              size={size}
+            />
+          );
+        },
+      })}
+    >
+      <Tab.Screen name='ホーム' component={HomeScreen} />
+      <Tab.Screen name='記録' component={RecordScreen} />
+      <Tab.Screen name='タスク' component={TaskScreen} />
+      <Tab.Screen name='マニュアル' component={ManualScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-blue-500 text-xl">Hello NativeWind!</Text>
-    </View>
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
   );
 }
 
